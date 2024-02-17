471786129a70cce8a5d4507a7423a30d5853f45a
bla <bla@bla.pl>
2024-02-17 11:42:38 +0100

    Pierwsza wersja aplikacji .net

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace uwsb_2024_net_webapp_ci_cd_azure.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
